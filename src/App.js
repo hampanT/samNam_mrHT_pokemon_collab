76b72c5cebddc55d20e5553cfde10c02ef535a70
hampanT <99350844+hampanT@users.noreply.github.com>
2024-10-27 13:32:33 +0100

    Inital frontend commit /ht :D

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import PokéDex from './images/pokedex.png'
+
+const PokemonImage = ({id}) => {
+  return (
+    <img
+      src={`${process.env.PUBLIC_URL}/images/showdown/${id}.gif`} 
+      alt={`Pokemon ${id}`}
+      />
+  );
+};
+
+const PokeCard = ({ id, name }) => {
+    return (
+      <div className="card">
+        <PokemonImage id={id}/>
+        <div className="card-wrapper">
+          <h3> {name} </h3>
+        </div>
+      </div>
+    )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={PokéDex} alt="pokedex"></img>
+      <div className="container">
+      <PokeCard id={2} name={"Test"}/>
+      <PokeCard id={10} name={"Test10"}/>
+      <PokeCard id={6} name={"Test"}/>
+      <PokeCard id={9} name={"Test"}/>
+      <PokeCard id={1} name={"Test"}/>
+      <PokeCard id={2} name={"Test10"}/>
+      <PokeCard id={3} name={"Test123"}/>
+      <PokeCard id={100} name={"Test123123123"}/>
+      </div>
     </div>
   );
 }
